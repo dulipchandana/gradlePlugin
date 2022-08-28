bea1ab95845459dcdd23201a2d90365d11c290ce
dulipchandana <dcdulip@gmail.com>
2022-08-29 00:10:05 +1000

    add file dif plugin

---
+++ file-diff-plugin/src/main/groovy/com/open/plugins/filediff/FileDiffPlugin.groovy
@@ -0,0 +1,16 @@
+package com.open.plugins.filediff
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FileDiffPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.extensions.create('fileDiff', FileDiffExtension)
+
+        project.tasks.register('fileDiff', FileDiffTask) {
+            file1 = project.fileDiff.file1
+            file2 = project.fileDiff.file2
+        }
+    }
+}
